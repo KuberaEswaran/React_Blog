007f91235b0d57438f655fef19ee21067be93c6c
Kubera_Eswaran_S <117847641+KuberaEswaran@users.noreply.github.com>
2025-01-14 12:23:03 +0530

    update footer postcount and db

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
   const postCount=useStoreState((state)=>state.postCount)
     return (
       <footer className="Footer">
-        {postCount} blog {postCount==0?"post":"posts"} 
+        {postCount} blog {postCount==1?"post":"posts"} 
         </footer>
     )
   }
